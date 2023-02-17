b29bec30e0f6130aa0e665fd84046872e8232c4a
chengy <1422988904@qq.com>
2023-02-17 09:25:39 +0800

    2023.2.17

--- runAll.py
+++ runAll.py
@@ -1,4 +1,6 @@
 import os,sys
+dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(dir)
 sys.path.append("D:\\apache-tomcat-9.0.41\\apache-tomcat-9.0.41\\webapps\\NEW_jenkins\\workspace\\test-jiekou-project\\venv\\Lib\\site-packages")
 import unittest
 import HTMLTestRunner
